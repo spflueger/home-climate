a699ce7a11984fa21dafbac7d43ca050ae676c83
Stefan Pflueger <spfluege@gmail.com>
2021-03-04 23:16:57 +0100

    feat: enable deep sleep mode via watchdog timer

--- measurement_station/src/main.cpp
+++ measurement_station/src/main.cpp
@@ -1,5 +1,7 @@
 
 #include <Arduino.h>
+#include <avr/sleep.h>
+#include <avr/wdt.h>
 
 #include "hdc1080_driver.hpp"
 #include <RH_ASK.h>
@@ -11,6 +13,13 @@
 #define RH_RX_PIN 10  // not used, set to a non-existens pin
 #define RH_TX_PIN 1   // Transmit pin
 #define RH_PTT_PIN 10 // not used, set to a non-existens pin
+
+// time until the watchdog wakes the mc in seconds
+#define WATCHDOG_TIME 2 // 1, 2, 4 or 8
+
+// after how many watchdog wakeups we should collect and send the data
+#define WATCHDOG_WAKEUPS_TARGET                                                \
+  1 // 8 * 7 = 56 seconds between each data collection
 
 RH_ASK rh_driver(RH_SPEED, RH_RX_PIN, RH_TX_PIN, RH_PTT_PIN);
 HDC1080I2CDriver hdc1080;
@@ -47,12 +56,54 @@ void StackPaint(void) {
 #endif
 }
 
+void enableWatchdog() {
+  cli();
+
+  // clear the reset flag
+  MCUSR &= ~(1 << WDRF);
+
+  // set WDCE to be able to change/set WDE
+  WDTCR |= (1 << WDCE) | (1 << WDE);
+
+// set new watchdog timeout prescaler value
+#if WATCHDOG_TIME == 1
+  WDTCR = 1 << WDP1 | 1 << WDP2;
+#elif WATCHDOG_TIME == 2
+  WDTCR = 1 << WDP0 | 1 << WDP1 | 1 << WDP2;
+#elif WATCHDOG_TIME == 4
+  WDTCR = 1 << WDP3;
+#elif WATCHDOG_TIME == 8
+  WDTCR = 1 << WDP0 | 1 << WDP3;
+#else
+#error WATCHDOG_TIME must be 1, 2, 4 or 8!
+#endif
+
+  // enable the WD interrupt to get an interrupt instead of a reset
+  WDTCR |= (1 << WDIE);
+
+  sei();
+}
+
+void enterSleep(void) {
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN);
+  sleep_enable();
+
+  // Now enter sleep mode
+  sleep_mode();
+
+  // The program will continue from here after the WDT timeout
+  // First thing to do is disable sleep
+  sleep_disable();
+}
+
 void setup() {
   hdc1080.init();
 
   if (!rh_driver.init()) {
     // do something in case init failed
   }
+
+  enableWatchdog();
 }
 
 void loop() {
@@ -60,5 +111,14 @@ void loop() {
 
   rh_driver.send((uint8_t *)&data, sizeof(data));
   rh_driver.waitPacketSent();
-  delay(1000);
+
+  // deep sleep
+  for (uint8_t i = 0; i < WATCHDOG_WAKEUPS_TARGET; i++) {
+    enterSleep();
+  }
+}
+
+// watchdog ISR
+ISR(WDT_vect) {
+  // nothing to do here, just wake up
 }
