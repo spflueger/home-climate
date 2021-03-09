59948b67342941d149b89dc2a0a8ca513b732edc
Stefan Pflueger <spfluege@gmail.com>
2021-03-09 22:43:04 +0100

    feat: add battery level measurement via adc

--- measurement_station/src/main.cpp
+++ measurement_station/src/main.cpp
@@ -6,6 +6,9 @@
 #include "hdc1080_driver.hpp"
 #include <RH_ASK.h>
 
+// for debugging purposes
+#define USE_STACK_COUNTING 1
+
 // RadioHead bitrate in bit/s
 #define RH_SPEED 2000
 
@@ -13,6 +16,10 @@
 #define RH_RX_PIN 10  // not used, set to a non-existens pin
 #define RH_TX_PIN 1   // Transmit pin
 #define RH_PTT_PIN 10 // not used, set to a non-existens pin
+
+// min max values for the ADC to calculate the battery percent
+#define ADC_MIN 600 // ~1.5V (3*1V/2)
+#define ADC_MAX 840 // ~2.1V (3*1.4V/2)
 
 // time until the watchdog wakes the mc in seconds
 #define WATCHDOG_TIME 2 // 1, 2, 4 or 8
@@ -24,11 +31,12 @@
 RH_ASK rh_driver(RH_SPEED, RH_RX_PIN, RH_TX_PIN, RH_PTT_PIN);
 HDC1080I2CDriver hdc1080;
 
+#if USE_STACK_COUNTING
 extern uint8_t _end;
 extern uint8_t __stack;
 
-// static const uint8_t STACK_CANARY = 0xC5;
 #define STACK_CANARY 0xC5
+
 void StackPaint(void) __attribute__((naked)) __attribute__((optimize("O0")))
 __attribute__((section(".init1")));
 
@@ -54,6 +62,39 @@ void StackPaint(void) {
                  "    brlo .loop\n"
                  "    breq .loop");
 #endif
+}
+
+uint16_t availableStackSize() {
+  const uint8_t *p = &_end;
+  uint16_t c = 0;
+
+  while (*p == STACK_CANARY && p <= &__stack) {
+    p++;
+    c++;
+  }
+
+  return c;
+}
+#endif
+
+void setupADC() {
+  ADMUX = (1 << REFS2) |  // Sets ref. voltage to 2.56V internal
+          (1 << REFS1) |  // Sets ref. voltage to 2.56V internal
+          (0 << REFS0) |  // Sets ref. voltage to 2.56V internal
+          (0 << MUX3) |   // use ADC2 for input (PB4), MUX bit 3
+          (0 << MUX2) |   // use ADC2 for input (PB4), MUX bit 2
+          (1 << MUX1) |   // use ADC2 for input (PB4), MUX bit 1
+          (0 << MUX0);    // use ADC2 for input (PB4), MUX bit 0
+  ADCSRA = (1 << ADEN) |  // enable ADC
+           (1 << ADPS2) | // set prescaler to 64, bit 2
+           (1 << ADPS1) | // set prescaler to 64, bit 1
+           (0 << ADPS0);  // set prescaler to 64, bit 0
+
+  // disable ADC for powersaving
+  ADCSRA &= ~(1 << ADEN);
+
+  // disable analog comperator for powersaving
+  ACSR |= (1 << ACD);
 }
 
 void enableWatchdog() {
@@ -96,16 +137,38 @@ void enterSleep(void) {
   sleep_disable();
 }
 
-uint16_t stack_free_size(void) {
-  const uint8_t *p = &_end;
-  uint16_t c = 0;
+uint8_t batteryLevel() {
+  // In order to have a low power battery measurement, a pin of the attiny (here
+  // pin3/PB3) is used to output the battery voltage and serves as a on off
+  // switch of the current through the voltage divider. turn on PB3
+  digitalWrite(PB3, HIGH);
 
-  while (*p == STACK_CANARY && p <= &__stack) {
-    p++;
-    c++;
+  // enable the ADC
+  ADCSRA |= (1 << ADEN);
+
+  delay(10);
+
+  ADCSRA |= (1 << ADSC); // start ADC measurement
+  while (ADCSRA & (1 << ADSC))
+    ; // wait till conversion complete
+
+  int16_t adc = ADCL & (ADCH << 8);
+
+  // clear the ADIF bit by writing 1 to it
+  ADCSRA |= (1 << ADIF);
+
+  // disable the ADC (power saving during power off state)
+  ADCSRA &= ~(1 << ADEN);
+  // turn off pin3
+  digitalWrite(PB3, LOW);
+
+  uint8_t battery_percentage = 100 * (adc - ADC_MIN) / (ADC_MAX - ADC_MIN);
+  if (adc > ADC_MAX) {
+    battery_percentage = 100;
+  } else if (adc < ADC_MIN) {
+    battery_percentage = 0;
   }
-
-  return c;
+  return battery_percentage;
 }
 
 void setup() {
@@ -115,13 +178,29 @@ void setup() {
     // do something in case init failed
   }
 
+  // use PB3 pin as a voltage source for the battery measurement
+  pinMode(PB3, OUTPUT);
+  setupADC();
   enableWatchdog();
 }
 
-void loop() {
-  ClimateData data = hdc1080.measure();
+struct DataPackage {
+  ClimateData climate_data;
+  uint8_t battery_level;
+  uint8_t station_id;
+#if USE_STACK_COUNTING
+  uint8_t available_stack_size;
+#endif
+};
 
-  data.humidity = (float)stack_free_size();
+void loop() {
+  DataPackage data;
+  data.climate_data = hdc1080.measure();
+  data.battery_level = batteryLevel();
+  data.station_id = 33;
+#if USE_STACK_COUNTING
+  data.available_stack_size = (uint8_t)availableStackSize();
+#endif
 
   rh_driver.send((uint8_t *)&data, sizeof(data));
   rh_driver.waitPacketSent();
